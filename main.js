32fd6d3cb760f4fef824521863573b03afec8af3
Marc Benaiges <benaiges.marc@gmail.com>
2019-02-13 14:05:30 +0100

    splash master

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const main = () =>{
+  
+  const buildDom = (html) => {
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  };
+
+  const buildSplashScreen = () => {
+    const spalshScreen = buildDom(`
+    <section class="splash-screen"></section>
+    <h1>Eternal enemies</h1>
+    <button>Start</button>
+    </section>
+    `);
+  };
+
+  buildSplashScreen();
+} ;
+
+window.addEventListener('load',main);
