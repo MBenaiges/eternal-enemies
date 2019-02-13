86acd58c703404a1491b65602ac71843e6701900
Marc Benaiges <benaiges.marc@gmail.com>
2019-02-13 16:57:26 +0100

    commit

--- game.js
+++ game.js
@@ -1,35 +1,73 @@
 'use strict'
 
-class Game{
-  constructor(canvas){
-    this.canvas= canvas;
-    this.ctx = this.canvas.getContext('2d');
-    this.player;
-    this.enemies=[];
-  };
+class Game {
+   constructor(canvas) {
+       this.canvas = canvas;
+       this.ctx = this.canvas.getContext('2d');
+       this.player;
+       this.enemies = [];
+       this.isGameOver = false;
+   };
 
-  startLoop(){
-    console.log("out of loop");
-    const loop=()=> {
-      console.log("in the loop")
-      this.updateCanvas();
-      this.clearCanvas();
-      this.drawCanvas();
-      window.requestAnimationFrame(loop);
-    }
-    window.requestAnimationFrame(loop);  
-  };
+   startLoop() {
+       //console.log("out of loop");
 
-  updateCanvas(){
+       this.player = new Player(this.canvas, 3);
+       const loop = () => {
+           //console.log("in the loop")
+           if (Math.random() > 0.97) {
+               const y = Math.random() * this.canvas.height;
+               this.enemies.push(new Enemy(this.canvas, y))
+           }
+           this.checkAllCollisions();
+           this.updateCanvas();
+           this.clearCanvas();
+           this.drawCanvas();
+           if(!this.isGameOver){
+            window.requestAnimationFrame(loop);
+           }
+           
+       }
+       window.requestAnimationFrame(loop);
+   };
 
-  };
+   updateCanvas() {
+       this.player.update();
+       this.enemies.forEach((enemy) => {
+           enemy.update();
+       })
 
-  clearCanvas(){
-    this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
-  };
+   };
 
-  drawCanvas(){
+   clearCanvas() {
+       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+   };
 
-  }
+   drawCanvas() {
+       this.player.draw();
+       this.enemies.forEach((enemy) => {
+           enemy.draw();
+       })
 
+   }
+   checkAllCollisions() {
+       this.player.checkScreen();
+       this.enemies.forEach((enemy, index)=>{
+         if (this.player.checkCollisionEnemy(enemy, index)){
+           console.log("dead");
+           this.player.loseLive();
+           this.enemies.splice(index,1);
+           if (this.player.lives===0){
+            this.isGameOver=true; 
+            this.onGameOver();
+
+           }
+         };
+       });
+       //collisions
+   }
+
+   gameOverCallBack(callback){
+     this.onGameOver = callback;
+   }
 };
--- main.js
+++ main.js
@@ -26,18 +26,31 @@ const main = () => {
      <canvas></canvas>
    </section>
    `);
-       const width = document.querySelector('.game-screen'.offsetWidth)
-       const height = document.querySelector('.game-screen'.offsetHeight)
+       const width = document.querySelector('.game-screen').offsetWidth;
+       const height = document.querySelector('.game-screen').offsetHeight;
 
        const canvasElement = document.querySelector('canvas');
        canvasElement.setAttribute('width', width);
        canvasElement.setAttribute('height', height);
 
-       setTimeout(buildGameOver, 3000);
+
+       //setTimeout(buildGameOver, 3000);
 
        const game = new Game(canvasElement);
+       game.gameOverCallBack(buildGameOver);
+       
 
        game.startLoop();
+
+       const setPlayerDirection = (event) => {
+           if (event.code === 'ArrowUp') {
+               game.player.setDirection(-1);
+           } else if (event.code === 'ArrowDown') {
+               game.player.setDirection(1);
+           };
+       };
+
+       document.addEventListener('keydown', setPlayerDirection);
    };
 
    const buildGameOver = () => {
